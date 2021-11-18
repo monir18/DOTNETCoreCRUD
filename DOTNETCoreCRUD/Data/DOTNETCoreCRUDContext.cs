afb75ffd33b2d9aa34df92ed6bf5bb053e6d5fea
monir18 <monir.bu18@gmail.com>
2021-11-18 17:18:26 +0600

    Dot Net Core Books App

---
+++ DOTNETCoreCRUD/Data/DOTNETCoreCRUDContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DOTNETCoreCRUD.Models;
+
+namespace DOTNETCoreCRUD.Data
+{
+    public class DOTNETCoreCRUDContext : DbContext
+    {
+        public DOTNETCoreCRUDContext (DbContextOptions<DOTNETCoreCRUDContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DOTNETCoreCRUD.Models.BookViewModel> BookViewModel { get; set; }
+    }
+}
